7c8029c8fd6dbaa4a598ff5fc863bd85e6b207a4
Charles E. Lehner <cel@celehner.com>
2022-05-06 01:02:45 -0400

    Update script for Python v3

--- nodensrecords.py
+++ nodensrecords.py
@@ -11,5 +11,5 @@ def nodetoiprange(nn):
 for nn in range(8192):
     cidr = nodetoiprange(nn)
     rtrip = "{}.{}.{}.{}".format(cidr[0], cidr[1], cidr[2], cidr[3]+1)
-    print "nycmesh-{}-rtr A {}".format(nn, rtrip)
-    print "n{} NS {}".format(nn, "nycmesh-{}-rtr".format(nn))
+    print("nycmesh-{}-rtr A {}".format(nn, rtrip))
+    print("n{} NS {}".format(nn, "nycmesh-{}-rtr".format(nn)))
