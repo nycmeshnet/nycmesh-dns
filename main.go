94b8a07aa9cb87e38e9d84833c4aec8662338dc8
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-10-09 09:22:18 -0400

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"golang.org/x/crypto/sha3"
@@ -33,4 +34,15 @@ func readAFileAsString(path string) string {
 // Get the hash of a string using SHA3-512
 func sha3Using512OfGivenByte(content []byte) string {
 	return fmt.Sprintf("%x", sha3.Sum512(content))
+}
+
+// Check if the given IP address is valid.
+func isIPValid(providedIP string) bool {
+	return net.ParseIP(providedIP) != nil
+}
+
+// Ensure that the CIDR block is valid.
+func validateCIDR(ipWithCidr string) bool {
+	_, _, err := net.ParseCIDR(ipWithCidr)
+	return err == nil
 }
