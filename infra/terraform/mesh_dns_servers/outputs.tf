3adc3a6087ab011aa4de9b7b9ce74e3b13d2deb3
james-otten <jamesotten1@gmail.com>
2024-07-08 22:45:36 -0400

    no output

--- infra/terraform/mesh_dns_servers/outputs.tf
+++ infra/terraform/mesh_dns_servers/outputs.tf
@@ -1,7 +1,7 @@
-output "authoritative_instance_id" {
-    value = proxmox_vm_qemu.authoritative_dns_vm[0].vmid
-}
+#output "authoritative_instance_id" {
+#    value = proxmox_vm_qemu.authoritative_dns_vm[0].vmid
+#}
 
-output "recursive_instance_id" {
-    value = proxmox_vm_qemu.recursive_dns_vm[0].vmid
-}
+#output "recursive_instance_id" {
+#    value = proxmox_vm_qemu.recursive_dns_vm[0].vmid
+#}
