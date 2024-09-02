a494d84d65f9c9d6e9109c8473b430f140c7af07
james-otten <jamesotten1@gmail.com>
2024-09-02 17:29:31 -0400

    add records + ci

---
+++ sld/records.nycmeshconnect.net.tf
@@ -0,0 +1,27 @@
+resource "namedotcom_record" "record__240356243" {
+  domain_name = "nycmeshconnect.net"
+  host        = ""
+  record_type = "A"
+  answer      = "185.199.108.153"
+}
+
+resource "namedotcom_record" "record__240356247" {
+  domain_name = "nycmeshconnect.net"
+  host        = ""
+  record_type = "A"
+  answer      = "185.199.109.153"
+}
+
+resource "namedotcom_record" "record__240356249" {
+  domain_name = "nycmeshconnect.net"
+  host        = ""
+  record_type = "A"
+  answer      = "185.199.110.153"
+}
+
+resource "namedotcom_record" "record__240356250" {
+  domain_name = "nycmeshconnect.net"
+  host        = ""
+  record_type = "A"
+  answer      = "185.199.111.153"
+}
---
+++ sld/records.themesh.foundation.tf
@@ -0,0 +1,13 @@
+resource "namedotcom_record" "record__4980549" {
+  domain_name = "themesh.foundation"
+  host        = ""
+  record_type = "A"
+  answer      = "75.126.102.240"
+}
+
+resource "namedotcom_record" "record__4980550" {
+  domain_name = "themesh.foundation"
+  host        = "*"
+  record_type = "A"
+  answer      = "75.126.102.240"
+}
