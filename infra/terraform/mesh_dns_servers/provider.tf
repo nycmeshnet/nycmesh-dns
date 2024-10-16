06fc2bc18ff323fb213eb6a4daebb4e685422a28
james-otten <james-otten@users.noreply.github.com>
2024-10-15 20:27:21 -0400

    IaC (#109)
    * IaC
    
    * cleanup
    
    * new zones
    
    * Revert "new zones"
    
    This reverts commit c9c6febc50e2888c2d63350ebec4908440b20b79.

---
+++ infra/terraform/mesh_dns_servers/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.3.0"
+    }
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+  }
+}
