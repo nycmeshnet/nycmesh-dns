3b00228e4f79f90cd0dbfa63e82ec5b628461327
james-otten <james-otten@users.noreply.github.com>
2025-07-08 23:04:22 -0400

    Swap a and cname records for the nn zone (#260)
    * swap a and cname
    
    * Update generate_nn.py
    
    * add validations

--- generate_nn.py
+++ generate_nn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 header = """$TTL 300
-@  SOA   nycmesh-713-dns-auth-3 hostmaster.nycmesh.net. ( 2025052100 1d 2h 4w 1h )
+@  SOA   ( nycmesh-713-dns-auth-3 hostmaster.nycmesh.net. 2025052100 1d 2h 4w 1h )
 @  NS    nycmesh-713-dns-auth-3
 
 nycmesh-10-dns-auth-5 A 23.158.16.23
@@ -18,5 +18,5 @@ def get_nn_ip(nn_s):
 with open("nn.zone", "w") as fd:
     fd.write(header)
     for nn in range(1, 8001):
-        fd.write(f"nn{nn} A {get_nn_ip(str(nn))}\n")
-        fd.write(f"{nn} CNAME nn{nn}\n")
+        fd.write(f"{nn} A {get_nn_ip(str(nn))}\n")
+        fd.write(f"nn{nn} CNAME {nn}\n")
