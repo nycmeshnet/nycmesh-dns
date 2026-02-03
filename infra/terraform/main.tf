60887fd71dc0d2e3dca7c54ed1bb18e9fc15164e
james-otten <jamesotten1@gmail.com>
2026-02-02 22:37:25 -0500

    fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -22,6 +22,7 @@ provider "proxmox" {
   pm_tls_insecure     = true
   pm_debug            = true
   pm_log_enable       = true
+  pm_log_file         = "terraform-plugin-proxmox.log"
   pm_api_token_id     = var.proxmox_token_id
   pm_api_token_secret = var.proxmox_token_secret
 }
