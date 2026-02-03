e75d2e045069c297f83a230aa4a8e76211c7fd8a
james-otten <jamesotten1@gmail.com>
2026-02-02 22:33:41 -0500

    fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,6 +21,7 @@ provider "proxmox" {
   # TODO: Setup cert
   pm_tls_insecure     = true
   pm_debug            = true
+  pm_log_enable       = true
   pm_api_token_id     = var.proxmox_token_id
   pm_api_token_secret = var.proxmox_token_secret
 }
