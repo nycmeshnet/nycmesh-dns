3b46cc5ed39bc256110f54baaada848a943b3daa
james-otten <james-otten@users.noreply.github.com>
2026-03-04 22:33:47 -0500

    Update records.nycmeshops.net.tf

--- sld/records.nycmeshops.net.tf
+++ sld/records.nycmeshops.net.tf
@@ -1,28 +1,28 @@
 
 resource "namedotcom_record" "record__269573914" {
-    domain_name = "nycmeshops.net"
-    host = ""
-    record_type = "A"
-    answer = "185.199.108.153"
+  domain_name = "nycmeshops.net"
+  host        = ""
+  record_type = "A"
+  answer      = "185.199.108.153"
 }
 
 resource "namedotcom_record" "record__269579330" {
-    domain_name = "nycmeshops.net"
-    host = ""
-    record_type = "A"
-    answer = "185.199.109.153"
+  domain_name = "nycmeshops.net"
+  host        = ""
+  record_type = "A"
+  answer      = "185.199.109.153"
 }
 
 resource "namedotcom_record" "record__269579331" {
-    domain_name = "nycmeshops.net"
-    host = ""
-    record_type = "A"
-    answer = "85.199.110.153"
+  domain_name = "nycmeshops.net"
+  host        = ""
+  record_type = "A"
+  answer      = "85.199.110.153"
 }
 
 resource "namedotcom_record" "record__269579332" {
-    domain_name = "nycmeshops.net"
-    host = ""
-    record_type = "A"
-    answer = "185.199.111.153"
+  domain_name = "nycmeshops.net"
+  host        = ""
+  record_type = "A"
+  answer      = "185.199.111.153"
 }
